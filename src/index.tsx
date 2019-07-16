a2a78d21c26a0dbc3eef4a928ccccc898992eff8
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-16 01:35:57 -0400

    Explored the two different ways to initialize state object on class-based component (constructor and property) and how they are very different in TypeScript as opposed to JavaScript

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,52 @@ interface AppProps {
     color?: string;
 }
 
+/*
+    If using the constructor method of initializing state object, create an interface
+    defining the pieces of state and their type. This can be passed as a second generic
+    argument to 'React.Component' (see type definition file).
+
+    Since a 'state' property already exists on 'React.Component' and it is the parent class
+    of the component, initializing state as a property on the component will override the
+    existing type definition of the property.
+
+    Either way is fine as long as you stay consistent.
+
+1) Constructor approach:
+
+interface AppState {
+    counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+    constructor(props: AppProps) {
+        super(props);
+
+        this.state = { counter: 0 };
+    }
+*/
+
+// 2) Property approach:
 class App extends React.Component<AppProps> {
+    state = { counter: 0 };
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1 });
+    };
+
     render() {
-        return <div>{this.props.color}</div>;
+        return (
+            <div>
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        );
     }
 }
 
